d6aebbb9cebaad40be0cb2b754c8a88e30d130a0
Karissa <k@karissademi.com>
2014-11-12 15:08:49 -0500

    initial commit
    super basic theme from _underscores starter theme

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package phila-gov
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php phila_gov_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'phila-gov' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php phila_gov_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
