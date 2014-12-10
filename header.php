f112860ce10b5f95585e61f62f1ec0406b661feb
Aaron Ogle <atogle@gmail.com>
2014-12-10 11:38:32 -0500

    Add Google Analytics/Tag Manager code snippet. - Note: may be better served via a plugin.

--- header.php
+++ header.php
@@ -27,6 +27,16 @@
 
 <body <?php body_class(); ?>>
 
+<!-- Google Tag Manager [alpha.phila.gov] -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDM9KV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KDM9KV');</script>
+<!-- End Google Tag Manager -->
+
 <div id="page" class="hfeed site">
     <header id="masthead" class="site-header pure-g" role="banner">
         <div class="container">
