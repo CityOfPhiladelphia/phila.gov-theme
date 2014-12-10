eb82ab0ffe49431391b214b4fa379a46d27e318a
Karissa <k@karissademi.com>
2014-12-10 14:28:11 -0500

    remove text search on /browse

--- content-finder.php
+++ content-finder.php
@@ -8,15 +8,15 @@
 <?php
 if (is_post_type_archive('department_page')){
     echo '<h1 class="h2">What Department Are You Looking for?</h1>';
-}elseif (is_tax('topics'))  {
-   echo '<h1 class="h2">What Kind of Information Are You Looking for?</h1>';
-}
     ?>
-<div id="servinfo-list-container" class="pure-u-1 pure-u-md-2-3">
+    <div id="servinfo-list-container" class="pure-u-1 pure-u-md-2-3">
     <form class="pure-form">
             <input class="pure-input-1 search" type="text" placeholder="Filter results...">
     </form>
     <?php
-    if (is_tax('topics')){ ?>
-        <h2><?php display_current_selected_topic();?></h2>
-  <?php  } ?>
+}elseif (is_tax('topics'))  {
+       //echo '<h1 class="h2">What Kind of Information Are You Looking for?</h1>';
+    ?>    <div id="servinfo-list-container" class="pure-u-1 pure-u-md-2-3">
+        <h2>  <?php display_current_selected_topic(); ?> </h2>
+
+    <?php } 
