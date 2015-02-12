365b5088b8af29ebc28b63ff77a4cec9519be48f
Karissa <k@karissademi.com>
2015-02-12 10:30:11 -0500

    update department list template

--- front-page.php
+++ front-page.php
@@ -9,125 +9,119 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div class="pure-g home-top">
-                <div class="container">
-                    <section id="welcome" class="pure-u-md-1-2">
-                        <div class="s-box-right">
-                            <div class="search">
-                            <header>
-                                <h1>
-                                    What can we help you find?
-                                </h1>
-                            </header>
-                                <?php get_search_form(); ?>
-                                <div class="search-help">e.g. Property Information, Department of Revenue, Water Billing</div>
-                            </div>
-                            <div id="popular" class="pure-g">
-                                <div class="pure-u-1-4">
-                                    <a href="http://iframe.publicstuff.com/#?client_id=242" target="_blank">
-                                        <span class="fa-stack fa-3x">
-                                          <i class="fa fa-circle fa-stack-2x"></i>
-                                          <i class="fa fa-stack-1x fa-inverse"><h6>311</h6></i>
-                                        </span>
-                                        <h2>Submit a Request</h2>
-                                        <span class="accessible"> Opens in new window</span>
-                                    </a>
-                                </div>
-                                <div class="pure-u-1-4">
-                                    <a href="/browse/payments-and-taxes/pay-a-bill" target="_blank">
-                                       <span class="fa-stack fa-3x">
-                                          <i class="fa fa-circle fa-stack-2x"></i>
-                                          <i class="fa fa-credit-card fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                        <h2>Pay a bill</h2>
-                                        <span class="accessible"> Opens in new window</span>
-                                    </a>
-                                </div>
-                                 <div class="pure-u-1-4">
-                                     <a href="http://property.phila.gov" target="_blank">
-                                        <span class="fa-stack fa-3x">
-                                          <i class="fa fa-circle fa-stack-2x"></i>
-                                          <i class="fa fa-home fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                        <h2>Property Search</h2>
-                                         <span class="accessible"> Opens in new window</span>
-                                     </a>
-                                </div>
-                                  <div class="pure-u-1-4">
-                                    <a href="http://www.phila.gov/personnel//announce/current/index.html" target="_blank">
-                                        <span class="fa-stack fa-3x">
-                                          <i class="fa fa-circle fa-stack-2x"></i>
-                                          <i class="fa fa-briefcase fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                       <h2>Jobs</h2>
-                                        <span class="accessible"> Opens in new window</span>
-                                    </a>
-                                </div>
+      <div class="row home-top">
+        <section id="welcome" class="medium-12 columns">
+            <div class="s-box-right">
+              <div class="search">
+                <header>
+                    <h1>What can we help you find?</h1>
+                </header>
+                    <?php get_search_form(); ?>
+                    <div class="search-help">e.g. Property Information, Department of Revenue, Water Billing</div>
+                </div>
+                <div id="popular" class="row">
+                    <div class="small-6 columns">
+                        <a href="http://iframe.publicstuff.com/#?client_id=242" target="_blank">
+                            <span class="fa-stack fa-3x">
+                              <i class="fa fa-circle fa-stack-2x"></i>
+                              <i class="fa fa-stack-1x fa-inverse"><h6>311</h6></i>
+                            </span>
+                            <h2>Submit a Request</h2>
+                            <span class="accessible"> Opens in new window</span>
+                        </a>
+                    </div>
+                    <div class="small-6 columns">
+                        <a href="/browse/payments-and-taxes/pay-a-bill" target="_blank">
+                           <span class="fa-stack fa-3x">
+                              <i class="fa fa-circle fa-stack-2x"></i>
+                              <i class="fa fa-credit-card fa-stack-1x fa-inverse"></i>
+                            </span>
+                            <h2>Pay a bill</h2>
+                            <span class="accessible"> Opens in new window</span>
+                        </a>
+                    </div>
+                     <div class="small-6 columns">
+                         <a href="http://property.phila.gov" target="_blank">
+                            <span class="fa-stack fa-3x">
+                              <i class="fa fa-circle fa-stack-2x"></i>
+                              <i class="fa fa-home fa-stack-1x fa-inverse"></i>
+                            </span>
+                            <h2>Property Search</h2>
+                             <span class="accessible"> Opens in new window</span>
+                         </a>
+                    </div>
+                      <div class="small-6 columns">
+                        <a href="http://www.phila.gov/personnel//announce/current/index.html" target="_blank">
+                            <span class="fa-stack fa-3x">
+                              <i class="fa fa-circle fa-stack-2x"></i>
+                              <i class="fa fa-briefcase fa-stack-1x fa-inverse"></i>
+                            </span>
+                           <h2>Jobs</h2>
+                            <span class="accessible"> Opens in new window</span>
+                        </a>
+                    </div>
 
-                            </div><!--#popular -->
-                            </div>
-                        </section>
-                    <section id="services" class="pure-u-md-1-2">
-                        <div class="m-box">
-                            <?php
-                            /* temporary top-level topics list w/ descriptions */
-                               $args = array(
-                                    'orderby' => 'name',
-                                    'fields'=> 'all',
-                                    'parent' => 0,
-                                   'hide_empty'=> false
-                               );
-                              $terms = get_terms( 'topics', $args );
-                                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-                                     echo '<ul>';
-                                     foreach ( $terms as $term ) {
-                                         echo '<li><a href="/browse/' . $term->slug .  '"><h2>' . $term->name . '</h2>';
-                                         echo '<span>' . $term->description . '</span></a></li>';
-                                     }
-                                     echo '</ul>';
-                                    }
+                </div><!--#popular -->
+                </div>
+          </section>
+          <section id="services" class="medium-12 columns">
+            <div class="m-box">
+                    <?php
+                    /* temporary top-level topics list w/ descriptions */
+                       $args = array(
+                            'orderby' => 'name',
+                            'fields'=> 'all',
+                            'parent' => 0,
+                           'hide_empty'=> false
+                       );
+                      $terms = get_terms( 'topics', $args );
+                        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+                             echo '<ul>';
+                             foreach ( $terms as $term ) {
+                                 echo '<li><a href="/browse/' . $term->slug .  '"><h2>' . $term->name . '</h2>';
+                                 echo '<span>' . $term->description . '</span></a></li>';
+                             }
+                             echo '</ul>';
+                            }
 
-                                    ?>
-                        </div>
-                    </section>
-                </div><!--.container -->
-            </div><!--.pure-g -->
-
-            <div class="pure-g home-news">
-                <div class="container">
-                        <section id="news" class="s-box">
-                            <?php
-                                $args = array(
-                                    'post_type' => array ('news_post'),
-                                    'posts_per_page'    => 3,
-                                    'meta_key'          => 'phila_show_on_home',
-                                    //only show if "yes" is selected
-                                    'meta_value'     => '1'
-                                );
-                                $counter = 0;
-                                $news_query = new WP_Query($args);
-                                if ( $news_query->have_posts() ) : while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-
-                                <div class="pure-u-md-7-24">
-                                    <div class="story s-box">
-                                        <?php get_home_news(); ?>
-                                    </div>
-                                </div>
-                            <?php
-                                $counter++;
-                                if ($counter === 1 || $counter === 2){
-                                 echo '<div class="pure-u-1-24 visible-md hidden-xm hidden-sm"></div>';
-                                }
                             ?>
-                                <?php endwhile; ?>
+          </div>
+        </section>
+			</div><!--.row -->
 
-                                <?php else : ?>
-                                    <div class="alert">No news!</div>
-                                <?php endif; ?>
+      <div class="row home-news">
+        <section id="news" class="s-box">
+            <?php
+                $args = array(
+                    'post_type' => array ('news_post'),
+                    'posts_per_page'    => 3,
+                    'meta_key'          => 'phila_show_on_home',
+                    //only show if "yes" is selected
+                    'meta_value'     => '1'
+                );
+                $counter = 0;
+                $news_query = new WP_Query($args);
+                if ( $news_query->have_posts() ) : while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
 
-                        </section><!--#news-->
-                    </div><!--.pure-u-1-->
-                </div><!-- .pure-g -->
+                <div class="pure-u-md-7-24">
+                    <div class="story s-box">
+                        <?php get_home_news(); ?>
+                    </div>
+                </div>
+            <?php
+                $counter++;
+                if ($counter === 1 || $counter === 2){
+                 echo '<div class="pure-u-1-24 visible-md hidden-xm hidden-sm"></div>';
+                }
+            ?>
+                <?php endwhile; ?>
+
+                <?php else : ?>
+                    <div class="alert">No news!</div>
+                <?php endif; ?>
+
+        </section><!--#news-->
+          </div><!-- .row -->
 
                 <section id="active" class="pure-g">
                         <div class="container">
