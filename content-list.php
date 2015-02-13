b75aff148be7c5792d4875d837b8ab3b419630b5
Karissa <k@karissademi.com>
2015-02-13 16:00:56 -0500

    department page fix

--- content-list.php
+++ content-list.php
@@ -8,16 +8,15 @@
 
 <li>
 
-    <?php 
+    <?php
     if (is_tax('topics')){
         the_title( sprintf( '<a href="%s" rel="bookmark" class="item"><h2 class="h3">', esc_url( get_permalink() ) ), '</h2></a>' );
     }else //it's the department list so:
         {
-    ?><a href="<?php echo get_permalink(); ?>" class="item"><?php get_department_category(); ?></a>
+    ?><a href="<?php echo get_permalink(); ?>" class="item"><?php echo the_title(); ?></a>
     <?php
         echo '<p class="item-desc">' . the_dept_description() . '</p>';
     }
 
     ?>
 </li>
-     
