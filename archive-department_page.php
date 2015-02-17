c00265f80196febcba76bb720465673234214c66
Karissa <k@karissademi.com>
2015-02-17 10:52:07 -0500

    departments alpha

--- archive-department_page.php
+++ archive-department_page.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Archive for Department List
- * 
+ *
  * @package phila-gov
  */
 
@@ -18,38 +18,38 @@ get_header(); ?>
             </div>
             <div class="pure-g">
                 <div class="container">
-          
+
                     <div class="pure-u-1 results">
-                    
+
                         <?php get_template_part( 'content', 'finder' ); ?>
-                        
+
                         <ul class="list"><!-- ul for sortable listness -->
-                            <?php 
+                            <?php
                                 $type = 'department_page';
-                                $department_listing = new WP_Query(array( 
-                                        'post_type' => $type, 
-                                        'posts_per_page' => -1, 
-                                        'orderby' => 'name', 
-                                        'order'=> 'asc' 
+                                $department_listing = new WP_Query(array(
+                                        'post_type' => $type,
+                                        'posts_per_page' => -1,
+                                        'orderby' => 'title', 
+                                        'order'=> 'asc'
                                     )
                                 );
-                               
+
                                 if ( $department_listing->have_posts() ) : ?>
-                            
+
                                 <?php while ( $department_listing->have_posts() ) : $department_listing->the_post(); ?>
 
                                     <?php get_template_part( 'content', 'list' ); ?>
                             <hr>
-                                
+
                                 <?php endwhile; ?>
                                 <?php else : ?>
 
                             <?php endif;
                             ?>
-                            
+
                         </ul>
                     </div>
-			     
+
                    </div> <!-- #servinfo-list-container -->
                 </div>
             </div>
