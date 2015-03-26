936208d1a9b0356a5ff1418832f3b15a0c05b3bc
Karissa <k@karissademi.com>
2015-03-26 14:12:30 -0400

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,18 @@ var browse =
           /*
           * phila.gov/browse
           */
-          var pathArray = window.location.pathname.split( '/' );
-
-          var parentURL = pathArray[3];
-          var childURL = pathArray[4];
-
-          var parentClass = $('.parent-topics li').hasClass(parentURL);
-          var childClass = $('.parent-topics li').hasClass(childURL);
-          var theParentClassName = '.' + pathArray[2];
-          var theChildClassName = '.' + pathArray[3];
+          var pathArray = window.location.pathname.split( '/' ),
+              parentURL = pathArray[3],
+              childURL = pathArray[4],
+              parentClass = $('.parent-topics li').hasClass(parentURL),
+              childClass = $('.parent-topics li').hasClass(childURL),
+              theParentClassName = '.' + pathArray[2],
+              theChildClassName = '.' + pathArray[3],
+              topicList = document.getElementById('topic').getElementsByTagName('li'),
+              $parentTopicDescription = $('.parent-topics').find('.parent-description'),
+              $childDescription = $('.child-description'),
+              $currentParentLink = $('.parent.current-topic').find('a'),
+              $topic = $('#topic');
 
           if (pathArray.length == 3){
               $('.browse nav ul').remove();
@@ -33,11 +36,14 @@ var browse =
 
           //compare the url with the classname - if the url reflects the classname, remove everything else
 
-          var topicList = document.getElementById('topic').getElementsByTagName('li'),
-              $parentTopicDescription = $('.parent-topics').find('.parent-description'),
-              $childDescription = $('.child-description'),
-              $currentParentLink = $('.parent.current-topic').find('a'),
-              $topic = $('#topic');
+
+              function removeHiddenLinks() {
+                var $parentCurrentTopic = $('.parent.current-topic'),
+                    $subtopics = $(".subtopics");
+
+                $parentCurrentTopic.next().addClass("current-subtopic");
+                $subtopics.not(".current-subtopic").remove();
+              }
 
               if (pathArray.length == 3){
                   $('.parent-topics', $topic).find('li.child').remove();
@@ -51,10 +57,8 @@ var browse =
                   $currentParentLink.removeAttr('href');
                   $topic.removeClass('large-16');
                   $topic.addClass('large-16 subtopic-select');
+                  removeHiddenLinks();
 
-                  $('.parent.current-topic').next().addClass("current-subtopic");
-
-                  $(".subtopics").not(".current-subtopic").remove();
 
               }else if (childClass == childURL){
                   $('.parent-topics li', $topic ).not(theParentClassName).remove();
@@ -66,6 +70,8 @@ var browse =
                   $currentParentLink.addClass('back-to-topic');
                   $('.browse nav ul').remove();
                   $childDescription.remove();
+                  removeHiddenLinks();
+
               }//end else if
 
           var currentURL = window.location.pathname;
