e443fed31d3f728df92ec66881527ca0625e65e2
Gabriel Farrell <g@gsf.io>
2015-10-22 16:07:22 +0000

    Formatting cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,23 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
     sass: {
-        dist: {
-            options: {
-                style: 'compressed'
-            },
-            files: {
-                'css/styles.css': 'css/scss/base.scss'
-            }
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/styles.css': 'css/scss/base.scss'
         }
+      }
     },
     uglify: {
-       options: {
-         sourceMap: true
-       },
-       js: {
-         files: {
-           'js/phila-scripts.min.js':
-           [
+      options: {
+        sourceMap: true
+      },
+      js: {
+        files: {
+          'js/phila-scripts.min.js': [
              'js/dependencies/jquery-deparam.js',
              'js/dependencies/jquery.ba-hashchange.min.js',
              'js/dependencies/jquery.swiftype.search.js',
@@ -29,30 +27,28 @@ module.exports = function(grunt) {
              'js/dev/scripts.js',
              'js/dev/search.js',
              'js/dev/skip-link-focus-fix.js'
-         ]
-       }
-     }
-   },
-   watch: {
-     css: {
-         files: ['css/scss/*.scss'],
-         tasks: ['sass'],
-         options: {
-             spawn: false,
-         }
-       },
-     js: {
-         files: ['js/dev/*.js'],
-         tasks: ['uglify']
-       }
-   }
+          ]
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['css/scss/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      },
+      js: {
+        files: ['js/dev/*.js'],
+        tasks: ['uglify']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-
   grunt.registerTask('default', ['sass', 'uglify']);
-
 };
