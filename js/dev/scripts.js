aecc210305252558d7c978745f6fab2757554e7e
Karissa Demi <k@karissademi.com>
2015-11-06 16:33:41 -0500

    Make entire document row clickable

--- js/dev/scripts.js
+++ js/dev/scripts.js
@@ -8,3 +8,9 @@
 new List('filter-list', {
     valueNames: ['item', 'item-desc']
 });
+
+jQuery(document).ready(function($) {
+  $(".clickable-row").click(function() {
+      window.document.location = $(this).data("href");
+  });
+});
