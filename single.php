2da5307cb77b9bae8c299e5509af7f285b8566b1
Karissa Demi <k@karissademi.com>
2015-12-04 21:19:36 -0500

    Correct indents

--- single.php
+++ single.php
@@ -7,19 +7,17 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area row">
-		<main id="main" class="site-main small-24 columns" role="main">
+<div id="primary" class="content-area row">
+  <main id="main" class="site-main small-24 columns" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+  <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'partials/content', 'single' ); ?>
+    <?php get_template_part( 'partials/content', 'single' ); ?>
 
-			<?php //phila_gov_post_nav(); ?>
+  <?php endwhile; // end of the loop. ?>
 
-		<?php endwhile; // end of the loop. ?>
+  </main><!-- #main -->
+</div><!-- #primary -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	
 
 <?php get_footer(); ?>
